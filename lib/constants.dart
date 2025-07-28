9587881aa199dbdecba0f62a02a88f85f46cbd69
Justin <justinmorettomtl@gmail.com>
2025-07-27 21:24:06 -0400

    updated constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-final String OMDbApiKey = dotenv.env['OMDB_API_KEY']!;
+final String OMDbAPIKey = dotenv.env['OMDB_API_KEY']!;
 const String OMDbBaseUrl = 'http://www.omdbapi.com/';
